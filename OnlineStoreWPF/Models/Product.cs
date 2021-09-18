2820cd8fd55ef183db3a9520bb2aabd8e930f0d3
Kenan805 <idayatovkenan7@gmail.com>
2021-09-19 02:00:57 +0400

    End commit

---
+++ OnlineStoreWPF/Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStoreWPF
+{
+    public class Product: INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyRaised();
+            }
+        }
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyRaised();
+            }
+        }
+        private string imagePath;
+
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set
+            {
+                imagePath = value;
+                OnPropertyRaised();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyRaised([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+    }
+}
